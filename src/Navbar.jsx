c041959ce60e0810d6141ae02ebffd437befaa85
HyunJungOh <hjoh@selva-i.co.jp>
2018-08-11 08:55:31 +0900

    fist commit-1

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Navbar extends Component {
+    constructor(props) {
+      super(props);
+//      this.words = 'some third words';
+    }
+  
+    render() {
+      return <div></div>;
+    }
+} 
