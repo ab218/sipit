b36430d7bd5e5a194021bc1189ad82b0d94303e7
A B <ab218@live.com>
2018-08-12 16:02:18 -0700

    set up database/api calls, working on yelp fusion

---
+++ routes/Users.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res, next) => {
+    knex.select('*').from('users')
+      .then((results) => {
+        res.json(results)
+      })
+      .catch((e) => {
+        res.status(500).send(e);
+      })
+  });
+  return router;
+}
+
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-
-
-const PORT = process.env.API_PORT | 8081;       // this port needs to match the port in the webapack.config.js -> devServer -> proxy
-
+const knexConfig = require('./knexfile');
+const bodyParser = require('body-parser');
+const knex = require('knex')(knexConfig['development']);
+const PORT = process.env.API_PORT | 8081; // this port needs to match the port in the webapack.config.js -> devServer -> proxy
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
 
-// can be GETted through the webpack-dev-server at localhost:8080/api or whatever host/port makes sense
-app.get('/api', (req, res) => {
-  res.json({pretty: 'much', amazing: 'eh'});
+const usersRoutes = require("./routes/Users.js");
+
+app.use("/api/users", usersRoutes(knex));
+
+
+app.listen(PORT, () => {
+  console.log(`Server up`);
 });
-
-app.listen(PORT, () => {console.log(`Server up`);});
-
-
