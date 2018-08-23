6246cd7bd6fbceb2c6178f71ee5fa0bea167ea9b
A B <ab218@live.com>
2018-08-22 21:04:12 -0700

    clean up heroku

--- server.js
+++ server.js
@@ -109,13 +109,11 @@ app.get("/api/business/:id/reviews", function (req, res) {
 })
 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'src/public', 'index.html'));
 });
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/src/public/index.html'));
-// });
+
 
 app.use(history());
 
