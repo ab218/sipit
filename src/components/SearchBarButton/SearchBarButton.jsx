3742d5d1c228d4e1caa1e933ae8c99f5e3f9d68d
A B <ab218@live.com>
2018-10-12 23:21:00 -0700

    made searchbarbutton a mui iconbutton

--- src/components/SearchBarButton/SearchBarButton.jsx
+++ src/components/SearchBarButton/SearchBarButton.jsx
@@ -1,42 +1,24 @@
 import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import IconButton from '@material-ui/core/IconButton';
 import styles from './searchBarButtonStyles';
 import { searchCafes } from '../../redux/actions';
 
 class SearchBarButton extends Component {
-  onMouseOver() {
-    this.searchBtn.style.color = 'orange';
-  }
-
-  onMouseLeave() {
-    this.searchBtn.style.color = '#FFF';
-  }
-
   render() {
-    const { customSearchBtn, searchIcon } = styles;
+    const { searchIcon } = styles;
     const {
       cafeSearch, locationSearch, resultsSearch, myLatLng, page, searchCafes,
     } = this.props;
 
     return (
-      <button
-        type="submit"
+      <IconButton
+        className="fas fa-search"
+        aria-label="Search"
         onClick={e => searchCafes(e, cafeSearch, locationSearch, resultsSearch, myLatLng, page)}
-        style={customSearchBtn}
-      >
-        <i
-          className="fas fa-search"
-          ref={(div) => {
-            this.searchBtn = div;
-          }}
-          onMouseOver={() => this.onMouseOver()}
-          onFocus={() => this.onMouseOver()}
-          onMouseLeave={() => this.onMouseLeave()}
-          onBlur={() => this.onMouseLeave()}
-          style={searchIcon}
-        />
-      </button>
+        style={searchIcon}
+      />
     );
   }
 }
